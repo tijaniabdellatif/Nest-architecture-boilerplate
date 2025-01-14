4f9a6c9f1942224b3cec225331fdefc84994cdcc
abdellatif tijani <tijani.idrissi.abdellatif@gmail.com>
2025-01-14 17:41:08 +0100

    issue templates

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,14 +1,16 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+import { UserEntity } from './user.entity';
 
 it('can create an instance of auth service', async () => {
   //Fake copy of userService
-  const fakeUserService = {
+  const fakeUserService:Partial<UsersService> = {
     find: () => Promise.resolve([]),
     create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password }),
+      Promise.resolve({ id: 1, email, password} as UserEntity),
   };
+
   const module = await Test.createTestingModule({
     providers: [
       AuthService,
